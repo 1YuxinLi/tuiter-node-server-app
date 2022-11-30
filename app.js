4f859c36a12799c12a9d8ea2493774f9cb00295f
yuxin li <yuxinli@yuxins-MacBook-Pro.local>
2022-11-30 15:38:02 -0800

    a9 Mongo fa22

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ import UserController from "./controllers/users/users-controller.js";
 import TuitsController
     from "./controllers/tuits/tuits-controller.js";
 const app = express();
+
+const DB_CONNECTION_STRING = "mongodb+srv://giuseppi:supersecretpassword@cluster0.lorvya8.mongodb.net/?retryWrites=true&w=majority"
+
+import mongoose from "mongoose";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://localhost:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
+// load the mongoose library
+// connect to the tuiter database
+
+
 app.use(cors())
 app.use(express.json());
 TuitsController(app);
